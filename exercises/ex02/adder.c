f9b02149d47790557b4ee2f03f97b35b1932b7a7
Anusha Datar <anushadatar@gmail.com>
2019-02-08 11:36:20 -0500

    well-designed

--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -7,48 +7,50 @@ displays their sum.
 #include <stdlib.h>
 #include <string.h>
 
-void get_user_input(int* numbers, int* current_index);
-int compute_sum(int* numbers, int* current_index);
-
+/* Function declarations. Function-specific documentation is located above
+the individual implementation. */
+int* get_user_input(int numbers[], int* current_index);
 
 /* The maximum quantity of numbers that a user can input. */
 int MAX_AMOUNT=100;
 /* The maximum length of an individual integer a user can input. */
 int MAX_LENGTH=10;
 
-void get_user_input(int* numbers, int *current_index) {
+int * get_user_input(int numbers[], int *current_index) {
     char input[MAX_LENGTH];
     int complete = 0;
     printf("Please enter the next number. Use Ctrl + D to quit. \n");
     while (!complete) {
+        printf("Starting");
         if (*current_index < MAX_AMOUNT) {
-            scanf("%2s", input);
-           // fgets(*input, MAX_LENGTH,stdin);
-            int current_value = atoi(input);  
-            if (current_value != EOF) {
-                numbers[*current_index] = current_value;
-                *current_index += 1;
+            if (fgets(input, MAX_LENGTH,stdin) == NULL) {
+                printf("Input complete. Computing sum of numbers.");
+                return numbers;
             }
-            else {
-                printf("Input complete. Computing sum of numbers");
-                complete = 1;
+            int current_value = atoi(input);  
+            printf("current value is %d", current_value);
+            if (current_value != 0) {    
+                numbers[*current_index] = current_value;
+                printf("The number at the current index %d is %d\n", *current_index, numbers[*current_index]);
+                *current_index += 1;    
             }
         }
     }
-}
-int compute_sum(int* numbers, int* current_index) {
-    int sum = 0;
-    for (int i = 0; i < *current_index; i++) {
-        sum += numbers[*current_index];
-    }
-    return sum;
+    return numbers;
 }
 
+
 int main() {
-    int numbers[MAX_AMOUNT];
-    int current_index;
+    int nums[MAX_AMOUNT];
+    int* numbers = nums;
+    int current_index = 0;
+    int sum = 0;
+
     printf("Getting user input.");
-    get_user_input(numbers, &current_index);
-    int sum = compute_sum(numbers, &current_index);
+    numbers = get_user_input(numbers, &current_index);
+    for (int i = 0; i < current_index; i++) {
+        printf("%d\n", numbers[i]);
+        sum += numbers[i];
+    }
     printf("The sum of the numbers is %d", sum); 
-} 
+}
