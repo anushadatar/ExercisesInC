160d01d6dae6c44b86eeaa417f6e14e6fb11bf02
Anusha Datar <anushadatar@gmail.com>
2019-02-08 00:45:21 -0500

    stack.c

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -5,6 +5,33 @@ License: GNU GPLv3
 
 */
 
+/*
+Experiment 1 : This code should theoretically create an array of length SIZE,
+               print its address, populate each index with the number 42,
+               create another array, print its address, populate that array
+               with the numbers 0 through SIZE-1, and print out those values
+               line-by-line. In this case, that looks like the two addresses
+               followed by the numbers 0 through 4 with new lines in between
+               each individual printout.
+
+Experiment 2 : When attempting compilation, the program reports that the 
+               function returns the address of a local variable. 
+               This implies that the return type of foo (an integer pointer)
+               is the address of a local variable - something destroyed
+               when the function is terminated that no longer has any
+               significance to the program itself.
+
+Experiment 3 : I got a segmentation fault! This makes a lot of sense because
+               I tried to access memory that should no longe exist - so
+               when trying to print the value, it could not execute. 
+               Before I got the segmentation fault, it printed the addresses
+               of both arrays, and the addresses are identical. 
+
+Experiment 4 : I commented out hte print statements in both of the functions.
+               Nothing really changed, except that it no longer printed the
+               addresses. This makes sense.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,7 +41,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+   // printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
@@ -26,7 +53,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    //printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
